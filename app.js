ef5f39c6456db734b119a4fab1368e0ecbb4bdc2
Tanja Johansen <tj@civagaard.dk>
2018-03-13 17:16:31 +0100

    Update 1
    keep track of user
    user should have unique identifier

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 var Chat = (function () {    // Module's code here... })()
- /*
- Keep track of connected users in the chat
- - A user should have a unique identiﬁer (e.g. email or a username),
- otherwise they should not be allowed to join
- - A user also needs to be able to leave
-
- Keep a record of all the messages
-
-Let users send a message to the chat
-- A message need to contain: A message, a send date and an author
-
-Search within messages
-- By content and by author
-
-Censor messages containing certain words
-- Censored should still be saved
-*/
 
   //user
   let users = []
 
   let module = {}
 
-  module.joinChat = function (user) {
-    console.log('User is joining', user)
-    users.push(user)
+ module.joinChat = function (user) {
 
-  }
-  module.helloWorld = function () {
-    console.log ('Hello World')
-  }
+   console.log ('User is trying to join:', user)
 
+let userAlreadyConnected = false
+
+  for (let i = 0; i < users.length; i++) {
+    //check if the user who is joining already is connected
+    if (users[i] == user) {
+     userAlreadyConnected = true
+  }
+}
+
+ if (userAlreadyConnected) {
+   console.log('User aready connected:', user)
+} else {
+  users.push(user)
+//add the user to connected users
+
+}
+}
+
+module.sendChat = function (message) {
+  //check if bad word is in massage
+
+  //if not, push the message
+  message.push(message)
+}
   return module
 
 })()
+
+Chat.joinChat('Sonia');
+Chat.joinChat('Tanja');
