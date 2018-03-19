6cac6330d0d9c27de13eba1eb1b3577551bdb132
Tanja Johansen <tj@civagaard.dk>
2018-03-19 10:26:21 +0100

    Update 4
    •	Keep track of connected users in the chat – this should be working
    o	A user should have a unique identiﬁer (e.g. email or a username), otherwise they should not be allowed to join – this should work
    •	A user also needs to be able to leave – we have a function for it, but we do not know if it actually works
    •	Keep a record of all the messages – this we are missing!!!
    •	Let users send a message to the chat – we have the function for it, but not sure if it works properly
    o	A message need to contain: A message, a send date and an author – we have it in the function, but I am not sure if it is being displayed
    •	Search within messages – we have the ‘keyword’ as part of function (indexOf), but we are not sure it works.
    o	By content and by author – included in code, but not sure it works.
    •	Censor messages containing certain words – this function we have, however it can only detect the bad word when it is not in a sentence. Also, it does not work when we add the message function.
    o	Censored should still be saved – do not know how to do this!!!

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var Chat = (function () {
-///////////////////////////////////////////////////////////////////////////////
- //////////////////////////// Users///////////////////////////////////////////
+ // Users
  let users = []
-
  let module = {}
 
  module.joinChat = function (user) {
-
    console.log ('User is trying to join:', user)
 
  let userAlreadyConnected = false
@@ -27,18 +24,19 @@ var Chat = (function () {
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////send message and serch for word///////////////
+//We have the elements, but something does not work. 
 let message = []
-
 function ChatMessage(message, user) {
   this.message = messages
   this.user = users
   this.createdAt = new Date()
 
-let newMessage1 = new ChaMessage('Hello World!', 'tanja')
-let newMessage2 = new ChaMessage('Hello Hello', 'tanja')
-let newMessage3 = new ChaMessage('Hello', 'tanja')
-let newMessage4 = new ChaMessage('2', 'tanja')
+let newMessage1 = new ChatMessage('Hello World!', 'Sonia')
+let newMessage2 = new ChatMessage('Hello Hello', 'Sonia')
+let newMessage3 = new ChatMessage('Hello', 'Sonia')
+let newMessage4 = new ChatMessage('2', 'Sonia')
 }
+
 message.push(newMessage1)
 message.push(newMessage2)
 message.push(newMessage3)
@@ -46,30 +44,51 @@ message.push(newMessage4)
 message.push(newMessage5)
 
 console.log('All messages', message)
+console.log('All users', user) // Did something
 
 //when we want to search, in this case it is the number 2
-let keyword = "Hello"
-
+let keyword = keyword.split('users', 'message') // Did something
 
 let results = messages.filter(m=> {
   // does this current message match whan i'm looking for?
   // let keywords = keyword.split('')
   return m.message.indexOf(keyword) !== -1
 
+let results = users.filter(u=> { //Did something
+
+  return u.users.indexOf(keyword) !== -1 //Did something
+  //search function with lowercase and uppercase letters. DOES NOT WORK!////////////////////////////
+
+  let searchBar = document.forms['search-users'].querySelector("#myInput");
+  searchBar.addEventListener('keyup',function(e){
+    let term = e.target.value.toLowerCase(); //Now we are comparing lowercase latters to lowercase letters.
+  //now I want to go trhough the array of authors/users and compare it with the searchBar
+
+  const authors = users.getElementsByTagName('li');
+  //Now we want to know if the searchterm is contained with the titels of the authors/users, by turning this into an array:
+  Array.from(users).forEach(function(user){
+    let message = users.firstElementChild.textContent;
+    //Now we want to check if the term is in the message
+    if(message.ToLowerCase().indexOf(term)!= -1) {
+      user.style.display = 'block';
+      //now we want to make an else statement that can hide the message if it is = -1:
+    } else {
+      user.style.display = 'none';
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+})
+
 })
 
 console.log('Message results', results)
 
-
-function logout (){
-  console.log('leaving chat');
-}
 })
 
-////////////////////////////////////////////////////////////////////////////////////////
-////////////////// Censor messages containing certain words/////////////////////////////////
-
-let messages = ['badword']
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////// Censor messages containing certain words////////////////////////////////////
+// This part only works when the above part (function chatMessage) is deleted. It seems they cannot both work at the same time. This also only works with one word at a time. If a whole sentence is written, the bad word will not be detected.
+let messages = ['shit','damn']
 
 module.sendChat = function (message) {
   console.log ('A message is written:', message)
@@ -90,26 +109,23 @@ if(badWordInMessage) {
 }
 }
 
- return module
+return module
 
 })()
-
-
-
-////////////////////////////////////////////////////////////////////////
 
 Chat.joinChat('Sonia');
 Chat.joinChat('Tanja');
 
 Chat.sendChat('Hello');
-Chat.sendChat('badword');
-
+Chat.sendChat('shit');
+Chat.sendChat('damn');
 
 //Chat.sendMessage('Hallo, my name is Sonia');
-
 //Logout function
+// We are not sure if this actually works
+function logout(){
+  console.log('leaving chat');
 
+}
 
-//function sendMessage (){
-  //console.log('Sending message');
-//}
+//})
